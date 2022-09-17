1a52a08aa6c74065b5472ac5f6a122f86e81603c
SamarV-121 <samarvispute121@pm.me>
2022-09-17 14:45:43 +0100

    pissarro: Rename libwifi-hal.so * To prevent conflict with aosp's
    Change-Id: I922f26c45957f4bc5d2ad85e14ecfcb382abf811
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- device.mk
+++ device.mk
@@ -273,7 +273,7 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi.hostapd@1.2.vendor
 
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service-lazy
+    android.hardware.wifi@1.0-service-lazy.pissarro
 
 PRODUCT_PACKAGES += \
     libkeystore-wifi-hidl \
