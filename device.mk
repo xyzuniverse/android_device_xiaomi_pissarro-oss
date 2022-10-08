a45548e620fdc40eb1444e27081b5c651abdad5d
TheMalachite <eliasgheeraert@gmail.com>
2022-10-08 13:10:52 +0300

    pissarro: Add persist.log.tag properties overrides
    Change-Id: Ibd6a89cbc0fa17b36fff1a0759afb64705687d13
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- device.mk
+++ device.mk
@@ -216,6 +216,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libtextclassifier_hash.vendor
 
+# Properties
+include $(LOCAL_PATH)/vendor_logtag.mk
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
