161bcc277ee5bad56d0dfc9f3320c6531d8ef908
xyzuniverse <dev.xyzuniverse@gmail.com>
2023-05-18 05:10:21 +0000

    pissarro: Define bootloader and platform flags
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,5 +21,12 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := mt6877
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6877
+
 # Inherit the proprietary files
 include vendor/xiaomi/pissarro/BoardConfigVendor.mk
